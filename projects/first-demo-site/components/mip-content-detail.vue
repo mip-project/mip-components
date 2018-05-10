209d7a354bfcc1301b956d21b33e44a9cccf008f
chenqiushi <chenqiushi@baidu.com>
2018-05-10 13:34:38 +0800

    增加一个测试项目

---
+++ projects/first-demo-site/components/mip-content-detail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="wrapper">
+        <h2>{{name}}</h2>
+        <mip-user-info></mip-user-info>
+        <p class="detail">
+            {{postDetail}}
+        </p>
+    </div>
+</template>
+
+<style lang="stylus">
+.wrapper
+    margin 0 auto
+    color #333
+    padding 0 10px
+
+    h2
+        padding 20px 0px
+
+    .detail
+        margin 20px 0
+        font-size 16px
+        line-height 28px
+</style>
+
+<script>
+    import mipUserInfo from './mip-user-info';
+    export default {
+        components: {
+            mipUserInfo
+        },
+        data () {
+            return {
+                name: '传统红烧肉',
+                postDetail: '在网上常看到，最家常的一道菜“红烧肉”，制作的工序各有不同，可以说是五花八门，在制作时，往里面放什么的都有。可最近有网友问我，“到底最正宗的红烧肉要如何来制作，是否能推荐一款？”其实，我也拿捏不准，也不知道谁的正宗，谁的不正宗！'
+            }
+        },
+        mounted() {
+        }
+    }
+</script>
---
+++ projects/first-demo-site/components/mip-item-list.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="list">
+        <h3>配料说明</h3>
+        <ul>
+            <li v-for="item in model.listData" @click="listClick(item.name)">
+                {{item.name}}<span>{{item.count}}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+.list
+    padding 0 10px 50px 10px
+    h3
+        padding 10px 0
+    li
+        padding 0 10px
+        line-height 36px
+        font-size 14px
+        border-bottom 1px solid #f0f0f0
+        span
+            color #999
+            float right
+</style>
+
+<script>
+    export default {
+        props: {
+            model: {
+                default() {
+                    return {};
+                },
+                type: Object
+            }
+        },
+        methods: {
+            listClick(name) {
+                console.log(name);
+            }
+        }
+    }
+</script>
