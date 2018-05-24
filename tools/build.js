0bb71bebabfa3c6f50bdab7d332920dec825d377
chenqiushi <chenqiushi@baidu.com>
2018-05-24 10:31:33 +0800

    移除 extensions

--- tools/build.js
+++ tools/build.js
@@ -7,19 +7,17 @@ const path = require('path');
 const fs = require('fs-extra');
 const execa = require('execa');
 
-const extensionsPath = path.resolve(__dirname, '../extensions');
+const extensionsPath = path.resolve(__dirname, '../components');
 
 if (!fs.pathExistsSync(extensionsPath)) {
-    console.log('ERR: Extensions dir does not exist! ');
+    console.log('ERR: components dir does not exist! ');
     return;
 }
 
 const build = function () {
-    let src = path.join(process.cwd(), 'extensions');
-    let dist = path.join(process.cwd(), 'dist');
     const publicPath = 'https://bos.nj.bpc.baidu.com/assets/mip/projects/common';
 
-    execa('mip2', ['build', '--dir', src, '--output', dist, '--asset', publicPath])
+    execa('mip2', ['build', '--asset', publicPath])
     .then(res => {
         console.log('All components has been build successfully!');
     });
